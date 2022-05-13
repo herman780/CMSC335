f5e538889e5616265638b3d2db33e5bc7a3cdd92
Herman Mann <70986277+herman780@users.noreply.github.com>
2022-05-12 21:08:05 -0400

    Create AppResources.java

---
+++ Project1/AppResources.java
@@ -0,0 +1,68 @@
+/*
+ * Filename: AppResources.java
+ * Author: Herman Mann
+ * Date: 03/27/2022
+ * Description: Class to handle the application related resource that are used across the application
+ */
+
+//Package
+package cmsc335_project1_hermanmann;
+
+/**
+ * Class to handle the application related resource that are used across the application
+ */
+public class AppResources {
+
+    public static final int TWO_DIMENSION = 2;
+    public static final int THREE_DIMENSION = 3;
+    public static final double PI = 3.14;
+    public static final int AREA_BY_FACTOR = 2;
+    public static final int VOLUME_BY_FACTOR = 3;
+    public static final int VOLUME_MULTIPLY_FACTOR = 4;
+    public static final int VOLUME_MULTIPLY_TWICE = 2;
+    public static final String MSG_CIRCLE = "Circle";
+    public static final String MSG_CONE = "Cone";
+    public static final String MSG_CUBE = "Cube";
+    public static final String MSG_CYLINDER = "Cylinder";
+    public static final String MSG_RECTANGLE = "Rectangle";
+    public static final String MSG_SPHERE = "Sphere";
+    public static final String MSG_SQUARE = "Square";
+    public static final String MSG_TORUS = "Torus";
+    public static final String MSG_TRIANGLE = "Rectangle";
+
+    public static final String MSG_HEADER = "\n\n***************Welcome to the JAVA OO Shapes Program*****************";
+    public static final String MSG_PRINT_SELECTED_OPTION = "You have selected a ";
+    public static final String MSG_PRINT_ENTER_RADIUS = "What is the radius?";
+    public static final String MSG_PRINT_AREA_FORMAT = "The area of the %s is %.2f";
+    public static final String MSG_PRINT_VOLUME_FORMAT = "The volume of the %s is %.2f";
+    public static final String MSG_PRINT_SELECTED_OPTION_FORMAT = "You have selected a %s";
+    public static final String MSG_PRINT_ENTER_LENGTH = "What is the length?";
+    public static final String MSG_PRINT_ENTER_WIDTH = "What is the width?";
+    public static final String MSG_PRINT_ENTER_SIDE = "What is the side?";
+    public static final String MSG_PRINT_ENTER_BASE = "What is the base?";
+    public static final String MSG_PRINT_ENTER_HEIGHT = "What is the height?";
+    public static final String MSG_PRINT_ENTER_MINOR_RADIUS = "What is the Minor Radius?";
+    public static final String MSG_PRINT_ENTER_MAJOR_RADIUS = "What is the Major Radius?";
+    public static final String MSG_PRINT_FOOTER_FORMAT = "Thanks for using the program. Today is %s %s at %s";
+    public static final String MSG_DATE_MONTH_FORMAT = "MMM";
+    public static final String MSG_DATE_DAY_FORMAT = "dd";
+    public static final String MSG_DATE_TIME_FORMAT = "hh:mm aa";
+    public static final String MSG_PRINT_CONTINUE = "\nWould you like to continue? (Y or N)";
+    public static final String MSG_PRINT_SELECTED_OPTION_ERROR = "Sorry I do not understand. Enter Y or N";
+    public static final char MSG_PRINT_CONTINUE_YES = 'Y';
+    public static final char MSG_PRINT_CONTINUE_NO = 'N';
+    public static final String MSG_PRINT_SELECTED_MENU_OPTION_ERROR = "Error - Menu choice is wrong. Please Enter choice between 1 to 10";
+
+    public static final String MSG_PRINT_SELECTED_MENU_OPTION = "Select from the menu below:";
+    public static final String MSG_PRINT_MENU_OPTION_1 = "1. Construct a Circle";
+    public static final String MSG_PRINT_MENU_OPTION_2 = "2. Construct a Rectangle";
+    public static final String MSG_PRINT_MENU_OPTION_3 = "3. Construct a Square";
+    public static final String MSG_PRINT_MENU_OPTION_4 = "4. Construct a Triangle";
+    public static final String MSG_PRINT_MENU_OPTION_5 = "5. Construct a Sphere";
+    public static final String MSG_PRINT_MENU_OPTION_6 = "6. Construct a Cube";
+    public static final String MSG_PRINT_MENU_OPTION_7 = "7. Construct a Cone";
+    public static final String MSG_PRINT_MENU_OPTION_8 = "8. Construct a Cylinder";
+    public static final String MSG_PRINT_MENU_OPTION_9 = "9. Construct a Torus";
+    public static final String MSG_PRINT_MENU_OPTION_10 = "10. Exit the program";
+
+}
