615a7f3e88e11dc31b24ae073364ad4926a71ead
Herman Mann <70986277+herman780@users.noreply.github.com>
2022-05-12 21:11:46 -0400

    Create AppResources.java

---
+++ Project3/AppResources.java
@@ -0,0 +1,69 @@
+/*
+ * Filename: AppResources.java
+ * Author: Herman Mann
+ * Date: 05/08/2022
+ * Description: Class to handle the application related resource that are used across the application
+ */
+//Package
+package cmsc335_project3_hermanmann;
+
+/**
+ * Class to handle the application related resource that are used across the
+ * application
+ */
+public class AppResources {
+
+    public static final String MSG_START = "Start";
+    public static final String MSG_PAUSE = "Pause";
+    public static final String MSG_STOP = "Stop";
+    public static final String MSG_CONTINUE = "Continue";
+
+    public static final String MSG_THREAD_1 = "Thread_1";
+    public static final String MSG_THREAD_2 = "Thread_2";
+    public static final String MSG_THREAD_3 = "Thread_3";
+
+    public static final String MSG_INTERSECTION_1 = "Intersection 1: ";
+    public static final String MSG_INTERSECTION_2 = "Intersection 2: ";
+    public static final String MSG_INTERSECTION_3 = "Intersection 3: ";
+
+    public static final String MSG_VEHICLE_1_THREAD = "Vehicle1Thread";
+    public static final String MSG_VEHICLE_2_THREAD = "Vehicle2Thread";
+    public static final String MSG_VEHICLE_3_THREAD = "Vehicle3Thread";
+    public static final String MSG_VEHICLE_4_THREAD = "Vehicle4Thread";
+
+    public static final String MSG_VEHICLE_1 = "Vehicle 1";
+    public static final String MSG_VEHICLE_2 = "Vehicle 2";
+    public static final String MSG_VEHICLE_3 = "Vehicle 3";
+    public static final String MSG_VEHICLE_4 = "Vehicle 4";
+
+    public static final String MSG_VEHICLE = "Vehicle";
+    public static final String MSG_X_POS = "X-Pos";
+    public static final String MSG_Y_POS = "Y-Pos";
+    public static final String MSG_SPEED = "Speed miles/h";
+    public static final String MSG_SPEED_UNIT = " miles/h";
+
+    public static final String MSG_RED = "Red";
+    public static final String MSG_GREEN = "Green";
+    public static final String MSG_YELLOW = "Yellow";
+
+    public static final String MSG_TITLE = "Herman Mann - CMSC 335 Project 3: Traffic Simulator";
+    public static final String MSG_WELCOME = "Herman Mann's  Traffic Simulator";
+    public static final String MSG_HEADER ="";
+
+    public static final String MSG_CURRENT_TIME = "Current time: ";
+
+    public static final String MSG_START_CALLING = " calling start";
+    public static final String MSG_SUSPEND_CALLING = " calling suspend";
+    public static final String MSG_RESUME_CALLING = " calling resume";
+    public static final String MSG_STOP_CALLING = " calling stop";
+
+    public static final String MSG_CREATING = "Creating ";
+    public static final String MSG_SUSPENDING = "Suspending ";
+    public static final String MSG_RESUMING = "Resuming ";
+    public static final String MSG_STARTING = "Starting ";
+    public static final String MSG_STOPPING = "Stopping ";
+    public static final String MSG_RUNNING = "Running ";
+    public static final String MSG_WAITING = " waiting";
+    public static final String MSG_TIME_FORMAT = "hh:mm:ss a";
+
+}
